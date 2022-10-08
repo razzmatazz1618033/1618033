4257871e58867943a967d6fe34c7aa6cb44ca3de
Kids22 <Kids22@G529-06>
2022-10-08 13:00:39 +0300

    Добавьте файлы проекта.

---
+++ 1618033/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _1618033
+{   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your Numbers: ");
+            var input = Console.ReadLine();
+            var numbers = input.Split(',');
+
+            foreach (var s in numbers)
+            {
+                Console.WriteLine(int.Parse(s)); // Вытаскивает число из строчки
+            }
+            Console.ReadLine();
+        }
+    }
+}
